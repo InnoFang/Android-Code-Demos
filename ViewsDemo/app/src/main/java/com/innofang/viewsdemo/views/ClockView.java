f37288dc9c70d01221be54b36cbbcdb7f1b137c2
InnoFang <q984941707@qq.com>
2017-04-19 16:31:50 +0800

    update ViewsDemo(implement ClockView)

--- ViewsDemo/app/src/main/java/com/innofang/viewsdemo/views/ClockView.java
+++ ViewsDemo/app/src/main/java/com/innofang/viewsdemo/views/ClockView.java
@@ -1,12 +1,15 @@
 package com.innofang.viewsdemo.views;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
-import android.view.View;
+import android.view.SurfaceView;
+
+import java.util.Calendar;
 
 /**
  * Author: Inno Fang
@@ -15,16 +18,25 @@ import android.view.View;
  */
 
 
-public class ClockView extends View
+public class ClockView extends SurfaceView
         implements SurfaceHolder.Callback, Runnable {
+
+    private static final String TAG = "ClockView";
 
     private Paint mArcPaint = new Paint();
     private Paint mTextPaint = new Paint();
+    private Paint mPointerPaint = new Paint();
+    private Paint mPointPaint = new Paint();
+    private Paint mPaint = new Paint();
+    private SurfaceHolder mSurfaceHolder;
     private int mHeight, mWidth;
     private boolean mDrawing = true;
-    private SurfaceHolder mSurfaceHolder;
-    private float mDailRadius = 400F;
-    private float len = 20F;
+    private float mDialRadius = 400F;
+    private float mLen = 20F;
+    private float mHourPointerLen = 150F;
+    private float mMinutesPointerLen = 200F;
+    private float mSecondsPointerLen = 270F;
+    private Bitmap mBitmapCache;
 
     public ClockView(Context context) {
         this(context, null);
@@ -36,8 +48,24 @@ public class ClockView extends View
         mArcPaint.setStyle(Paint.Style.STROKE);
         mArcPaint.setAntiAlias(true);
         mArcPaint.setColor(Color.BLACK);
+
+        mPointerPaint.setStrokeWidth(10);
+        mPointerPaint.setStyle(Paint.Style.STROKE);
+        mPointerPaint.setAntiAlias(true);
+
         mTextPaint.setTextSize(50);
-//        mSurfaceHolder = getHolder();
+        mTextPaint.setAntiAlias(true);
+
+        mPointPaint.setAntiAlias(true);
+        mPointPaint.setStrokeWidth(30);
+        mPointPaint.setColor(Color.BLACK);
+        mPointPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        mSurfaceHolder = getHolder();
+        mSurfaceHolder.addCallback(this);
     }
 
     @Override
@@ -50,7 +78,7 @@ public class ClockView extends View
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         mDrawing = true;
-//        new Thread(this).start();
+        new Thread(this).start();
     }
 
     @Override
@@ -64,19 +92,70 @@ public class ClockView extends View
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.translate(mWidth / 2, mHeight / 2);
-        drawDial(canvas);
-    }
-
-    @Override
     public void run() {
         while (mDrawing) {
             Canvas canvas = mSurfaceHolder.lockCanvas();
-
+            if (null == mBitmapCache) {
+                mBitmapCache = Bitmap.createBitmap(canvas.getWidth(),
+                        canvas.getHeight(),
+                        Bitmap.Config.ARGB_4444);
+            }
+            Canvas canvas1 = new Canvas(mBitmapCache);
+            canvas1.drawColor(Color.WHITE);
+            canvas1.translate(mWidth / 2, mHeight / 2);
+            drawCanvas(canvas1);
+            canvas.drawBitmap(mBitmapCache, 0, 0, mPaint);
             mSurfaceHolder.unlockCanvasAndPost(canvas);
         }
+    }
+
+    private void drawCanvas(Canvas canvas) {
+        if (null == canvas) return;
+        /* 画刻度盘 */
+        drawDial(canvas);
+        /* 画指针 */
+        drawPointer(canvas);
+        /* 画中心点 */
+        drawPoint(canvas);
+    }
+
+    private void drawPoint(Canvas canvas) {
+        canvas.drawPoint(0, 0, mPointPaint);
+    }
+
+    private void drawPointer(Canvas canvas) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int seconds = calendar.get(Calendar.SECOND);
+//        Log.i(TAG, hour + " : " + minute + " : " + seconds);
+        float rotateAngle = 0;
+
+        /* draw hour pointer */
+        canvas.save();
+        rotateAngle = hour * 30.0F; // 一小时一大格
+        int plusHour = minute / 12; // 12分钟一小格
+        rotateAngle += plusHour * 6.0F;
+        mPointerPaint.setColor(Color.RED);
+        canvas.rotate(rotateAngle);
+        canvas.drawLine(0, -20, 0, -mHourPointerLen, mPointerPaint);
+        canvas.restore();
+
+        /* draw minute pointer */
+        canvas.save();
+        rotateAngle = minute * 6.0F; // 一分钟一小格
+        canvas.rotate(rotateAngle);
+        mPointerPaint.setColor(Color.GREEN);
+        canvas.drawLine(0, -20, 0, -mMinutesPointerLen, mPointerPaint);
+        canvas.restore();
+
+        /* draw second pointer */
+        canvas.save();
+        rotateAngle = seconds * 6.0F; // 一秒钟一小格
+        canvas.rotate(rotateAngle);
+        mPointerPaint.setColor(Color.BLUE);
+        canvas.drawLine(0, -20, 0, -mSecondsPointerLen, mPointerPaint);
+        canvas.restore();
     }
 
     private void drawDial(Canvas canvas) {
@@ -92,11 +171,11 @@ public class ClockView extends View
                 }
                 mArcPaint.setColor(Color.RED);
                 mTextPaint.setColor(Color.BLACK);
-                canvas.drawLine(0, -mDailRadius, 0, -(mDailRadius + 2 * len), mArcPaint);
-                canvas.drawText(timeText, -15 /*让数子尽量居中*/, -(mDailRadius + len * 5 / 2), mTextPaint);
+                canvas.drawLine(0, -mDialRadius, 0, -(mDialRadius + 2 * mLen), mArcPaint);
+                canvas.drawText(timeText, -15 /*让数字尽量居中*/, -(mDialRadius + mLen * 5 / 2), mTextPaint);
             } else {
                 mArcPaint.setColor(Color.GREEN);
-                canvas.drawLine(0, -mDailRadius, 0, -(mDailRadius + len), mArcPaint);
+                canvas.drawLine(0, -mDialRadius, 0, -(mDialRadius + mLen), mArcPaint);
             }
             canvas.rotate(6);
         }
