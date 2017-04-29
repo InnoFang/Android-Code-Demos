24e87cac97b8a704b8ced855a39c87796b0a3889
InnoFang <q984941707@qq.com>
2017-04-29 15:01:00 +0800

    update TransitionDemo(implement Window Transition)

--- TransitionDemo/app/src/main/java/io/innofang/transitiondemo/CircularRevealActivity.java
+++ TransitionDemo/app/src/main/java/io/innofang/transitiondemo/CircularRevealActivity.java
@@ -4,10 +4,13 @@ import android.animation.Animator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 
 public class CircularRevealActivity extends AppCompatActivity {
+
+    private static final String TAG = "CircularRevealActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,22 @@ public class CircularRevealActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 secondView.setVisibility(View.VISIBLE);
+                /* 找到 FAB 的圆心(在屏幕坐标轴的坐标) */
                 int centerX = (v.getLeft() + v.getRight()) / 2;
                 int centerY = (v.getTop() + v.getBottom()) / 2;
+                /* Math.hypot(centerX, centerY) 返回 sqrt(centerX2 + centerY2) 的值*/
                 float finalRadius = (float) Math.hypot(centerX, centerY);
+                Log.i(TAG, "onClick: centerX " + centerX);
+                Log.i(TAG, "onClick: centerY " + centerY);
+                Log.i(TAG, "onClick: finalRadius " + finalRadius);
                 Animator mCircularReveal = ViewAnimationUtils.createCircularReveal(
-                        secondView, centerX, centerY, 0, finalRadius);
+                        secondView, /* 覆盖的 View */
+                        centerX,    /* 圆心横坐标 */
+                        centerY,    /* 圆心纵坐标 */
+                        0,          /* 圆形过度动画开始半径 */
+                        finalRadius /* 圆形过度动画结束半径 */
+                );
+                /* 开始动画，间隔为 400 毫秒 */
                 mCircularReveal.setDuration(400).start();
             }
         });
--- TransitionDemo/app/src/main/java/io/innofang/transitiondemo/WindowTransitionsSecondActivity.java
+++ TransitionDemo/app/src/main/java/io/innofang/transitiondemo/WindowTransitionsSecondActivity.java
@@ -1,41 +1,30 @@
 package io.innofang.transitiondemo;
 
-import android.app.ActivityOptions;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
-import android.view.View;
 import android.view.Window;
 
-public class WindowTransitionsActivity extends AppCompatActivity {
+public class WindowTransitionsSecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= 21)
             getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-        setContentView(R.layout.activity_window_transitions);
+        setContentView(R.layout.activity_window_transitions_second);
 
         Transition transition = TransitionInflater
                 .from(this)
-                .inflateTransition(R.transition.explode); /* 爆炸效果 */
+//                .inflateTransition(R.transition.explode); /* 爆炸效果 */
 //                .inflateTransition(R.transition.fade);  /* 淡化效果 */
-//                .inflateTransition(R.transition.slide); /* 滑动效果 */
+                .inflateTransition(R.transition.slide); /* 滑动效果 */
 
         /* 退出时使用 */
         getWindow().setExitTransition(transition);
-        /* 再次进入时使用 */
-        getWindow().setReenterTransition(transition);
         /* 第一次进入时使用 */
         getWindow().setEnterTransition(transition);
-    }
-
-    public void onCellClick(View view) {
-        startActivity(new Intent(this, WindowTransitionsSecondActivity.class),
-                /* 添加这句有一种淡入淡出的效果 */
-                ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 }
