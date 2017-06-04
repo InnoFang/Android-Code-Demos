6d115c1758edb4eacab342e424fd088d057cc934
InnoFang <q984941707@qq.com>
2017-06-04 11:31:46 +0800

    update Base Demo

--- BaseDemo/app/src/main/java/io/innofang/basedemo/base/BaseSimpleViewHolder.java
+++ BaseDemo/app/src/main/java/io/innofang/basedemo/base/BaseSimpleViewHolder.java
@@ -1,6 +1,9 @@
 package io.innofang.basedemo.base;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +11,7 @@ import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -17,7 +21,7 @@ import android.widget.TextView;
  */
 
 
-public class BaseSimpleViewHolder  extends RecyclerView.ViewHolder {
+public class BaseSimpleViewHolder extends RecyclerView.ViewHolder {
 
     private SparseArray<View> mViews;
     private View mItemView;
@@ -29,12 +33,12 @@ public class BaseSimpleViewHolder  extends RecyclerView.ViewHolder {
     }
 
     @SuppressWarnings("unchecked")
-    public static <T extends BaseSimpleViewHolder>T getViewHolder(Context context, ViewGroup parent, @LayoutRes int layoutId) {
+    public static <T extends BaseSimpleViewHolder> T getViewHolder(Context context, ViewGroup parent, @LayoutRes int layoutId) {
         return (T) new BaseSimpleViewHolder(LayoutInflater.from(context).inflate(layoutId, parent, false));
     }
 
     @SuppressWarnings("unchecked")
-    public <T extends View>T getView(@IdRes int id) {
+    public <T extends View> T getView(@IdRes int id) {
         View child = mViews.get(id);
         if (null == child) {
             child = itemView.findViewById(id);
@@ -52,4 +56,43 @@ public class BaseSimpleViewHolder  extends RecyclerView.ViewHolder {
             ((TextView) getView(id)).setText(text);
         }
     }
+
+    public void setTextColor(@IdRes int id, @ColorInt int color) {
+        if (getView(id) instanceof TextView) {
+            ((TextView) getView(id)).setTextColor(color);
+        }
+    }
+
+    public void setImageResource(@IdRes int id, @DrawableRes int resId) {
+        if (getView(id) instanceof ImageView) {
+            ((ImageView) getView(id)).setImageResource(resId);
+        }
+    }
+
+    public void setImageDrawable(@IdRes int id, Drawable drawable) {
+        if (getView(id) instanceof ImageView) {
+            ((ImageView) getView(id)).setImageDrawable(drawable);
+        }
+    }
+
+    public void setBackgroundColor(@IdRes int id, @ColorInt int color) {
+        getView(id).setBackgroundColor(color);
+    }
+
+    public void setVisible(@IdRes int id, boolean visible) {
+        getView(id).setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    public void setOnClickListener(int viewId, View.OnClickListener listener) {
+        getView(viewId).setOnClickListener(listener);
+    }
+
+    public void setOnTouchListener(int viewId, View.OnTouchListener listener) {
+        getView(viewId).setOnTouchListener(listener);
+    }
+
+    public void setOnLongClickListener(int viewId, View.OnLongClickListener listener) {
+        getView(viewId).setOnLongClickListener(listener);
+    }
+
 }
--- BaseDemo/app/src/main/java/io/innofang/basedemo/module/MainFragment.java
+++ BaseDemo/app/src/main/java/io/innofang/basedemo/module/MainFragment.java
@@ -1,5 +1,6 @@
 package io.innofang.basedemo.module;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import io.innofang.basedemo.R;
 import io.innofang.basedemo.base.BaseFragment;
@@ -47,10 +49,26 @@ public class MainFragment extends BaseFragment {
         }
         RecyclerView rv = (RecyclerView) find(R.id.rv);
         rv.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rv.setAdapter(new BaseSimpleAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list) {
+        rv.setAdapter(new BaseSimpleAdapter<String>(getActivity(), android.R.layout.activity_list_item, list) {
             @Override
             protected void bindViewHolder(BaseSimpleViewHolder viewHolder, String s, int position) {
+                viewHolder.setImageResource(android.R.id.icon, R.mipmap.ic_launcher_round);
                 viewHolder.setText(android.R.id.text1, s);
+                viewHolder.setBackgroundColor(android.R.id.text1, Color.GRAY);
+                viewHolder.setTextColor(android.R.id.text1, Color.RED);
+                viewHolder.setOnClickListener(android.R.id.text1, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toasts.showShort(((TextView)v).getText().toString());
+                    }
+                });
+
+                Random random = new Random();
+                if (random.nextInt() % 2 == 0) {
+                    viewHolder.setVisible(android.R.id.icon, true);
+                } else {
+                    viewHolder.setVisible(android.R.id.icon, false);
+                }
             }
         });
     }
