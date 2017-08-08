e8467043c24552754438bd5f159452fb3a5cb204
InnoFang <q984941707@qq.com>
2017-08-08 09:34:16 +0800

    add ModularizationDemo

---
+++ ModularizationDemo/app/src/main/java/io/innofang/modularizationdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package io.innofang.modularizationdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import io.innofang.modulea.ModuleAActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(MainActivity.this, ModuleAActivity.class));
+    }
+}
---
+++ ModularizationDemo/moduleA/src/main/java/io/innofang/modulea/ModuleAActivity.java
@@ -0,0 +1,13 @@
+package io.innofang.modulea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModuleAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_a);
+    }
+}
