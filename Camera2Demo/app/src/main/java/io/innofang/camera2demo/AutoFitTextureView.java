66d8546cbd880ff1ac127f33a93b7816cfd93945
InnoFang <q984941707@qq.com>
2017-09-24 22:10:47 +0800

    upload Camera2Demo

---
+++ Camera2Demo/app/src/main/java/io/innofang/camera2demo/AutoFitTextureView.java
@@ -0,0 +1,73 @@
+package io.innofang.camera2demo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+/**
+ * Author: Inno Fang
+ * Time: 2017/9/24 20:37
+ * Description:
+ */
+
+
+public class AutoFitTextureView extends TextureView {
+
+    private int mRatioWidth = 0;
+    private int mRatioHeight = 0;
+
+    public AutoFitTextureView(Context context) {
+        super(context, null);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public AutoFitTextureView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setAspectRatio(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+
+        mRatioHeight = height;
+        mRatioWidth = width;
+        requestLayout();
+    }
+
+    /**
+     * 根据设置的长宽比重新定义长宽
+     *        实际长     比率长
+     * 公式 ：------ = ------
+     *        实际宽     比率宽
+     *
+     *                  比率长
+     * 实际长 = 实际宽 * ------
+     *                  比率宽
+     *
+     *                  比率宽
+     * 实际宽 = 实际长 * ------
+     *                  比率长
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        if (0 == mRatioWidth || 0 == mRatioHeight) {
+            setMeasuredDimension(width, height);
+        } else {
+            if (width < height * mRatioWidth / mRatioHeight) { // height 值过高
+                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+            } else {
+                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+            }
+        }
+    }
+}
---
+++ Camera2Demo/app/src/main/java/io/innofang/camera2demo/MainActivity.java
@@ -0,0 +1,19 @@
+package io.innofang.camera2demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+
+}
